ab04e14db979afa99b0754ef7a3a0d99a0363da9
najeebib <najeebibraheem1999@hotmail.com>
2024-07-25 21:33:36 +0300

    connected the bot to server with question functionality

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -1,5 +1,9 @@
 # bot/config/settings.py
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
+    SERVER_ADDRESS = os.getenv('SERVER_ADDRESS')
--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,10 @@
-import os
 import requests
-from dotenv import load_dotenv
-from telegram.ext import Application, CommandHandler
-from bot.handlers.basic_fns import start, connect
+from telegram.ext import Application, CommandHandler, MessageHandler, ConversationHandler, filters, ContextTypes
+from bot.handlers.basic_fns import start, connect, help, question_command, difficulty_response, answers_response, topic_response, cancel
+from bot.config.settings import Settings
 
 # Load environment variables from .env file
-load_dotenv()
+DIFFICULTY, ANSWERS, TOPIC, USER_ANSWER = range(4)
 
 def get_public_ip():
     response = requests.get('https://api.ipify.org?format=json')
@@ -18,15 +17,30 @@ def main():
 
     # Create the Application and pass it your bot's token
     try:
-        BOT_TOKEN = os.getenv('BOT_TOKEN')
+        BOT_TOKEN = Settings.BOT_TOKEN
         if BOT_TOKEN:
             application = Application.builder().token(BOT_TOKEN).build()
             # Register the /start command with the start function
             start_handler = CommandHandler('start', lambda update, context: start(update, context, public_ip))
             connect_handler = CommandHandler('connect', lambda update, context: connect(update, context))
+            help_handler = CommandHandler('help', lambda update, context: help(update, context))
+
+            conv_handler = ConversationHandler(
+                entry_points=[CommandHandler('question', question_command)],
+                states={
+                    DIFFICULTY: [MessageHandler(filters.TEXT & ~filters.COMMAND, difficulty_response)],
+                    ANSWERS: [MessageHandler(filters.TEXT & ~filters.COMMAND, answers_response)],
+                    TOPIC: [MessageHandler(filters.TEXT & ~filters.COMMAND, topic_response)]
+                },
+                fallbacks=[CommandHandler('cancel', cancel)],
+            )
+
+            application.add_handler(conv_handler)
+
 
             application.add_handler(start_handler)
             application.add_handler(connect_handler)
+            application.add_handler(help_handler)
 
             # Start the Bot
             application.run_polling()
