53144ebc57aced11cb0efc1c1abe54b9b23f73de
muhammad_Sarahni <117571362+muhammadser1@users.noreply.github.com>
2024-07-29 14:24:04 +0300

    Delete settings.py

--- bot/config/settings.py
+++
@@ -1,9 +0,0 @@
-# bot/config/settings.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Settings:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    SERVER_ADDRESS = os.getenv('SERVER_ADDRESS')
