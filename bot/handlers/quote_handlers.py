deaa8ba0da85fde97c829c594b90a4bc7656953f
najeebib <najeebibraheem1999@hotmail.com>
2024-08-07 12:32:09 +0300

    fixed issue

--- bot/handlers/quote_handlers.py
+++ bot/handlers/quote_handlers.py
@@ -7,6 +7,5 @@ from bot.setting.config import config
 async def quote_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.message.from_user.id
     response = requests.get( f"{config.SERVER_URL}/quote/{user_id}").json()
-    quote = response[0]["quote"]
-
+    quote = response["quote"]
     await update.message.reply_text(quote)
